6da28685e64206e9e2bc9f1525ec9125fdf87637
benqbax <ben_forsrup@hotmail.com>
2017-10-01 11:33:41 +0200

    Fixed currentMessage depending on move

--- lab3/src/FifteenModel.java
+++ lab3/src/FifteenModel.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class FifteenModel implements Boardgame {
     private String currentMessage = "No message yet";
     private String[][] status = new String[4][4];  // spelplanen
@@ -27,47 +29,76 @@ public class FifteenModel implements Boardgame {
 
     }
 
+    //https://stackoverflow.com/questions/20389890/generating-a-random-number-between-1-and-10-java
+    public static int randInt(int min, int max) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+        return randomNum;
+    }
     private void shuffleBoard(){
-
+        
     }
 
-    private boolean inBounds(int x){
+    private static boolean inBounds(int x){
         boolean condition = (x >= 0) && (x < 4);
         return condition;
     }
 
     @Override
     public boolean move(int i, int j) {
-        System.out.println(status[i-1][j]);
-        boolean inBoundsX =inBounds(j+1);
+        if(!inBounds(i) && !inBounds(j)){
+            currentMessage = "Please choose a position on the board!";
+            return false;
+        }
 
         if(inBounds(i+1) && status[i+1][j] == null){
-            System.out.println("good move1");
+
+            currentMessage="OK";
+
+            //move the numbers
             status[i+1][j] = status[i][j];
             status[i][j] = null;
+
+            //set the blank variables
+            xemp = i;
+            yemp = j;
+
         }
         else if(inBounds(i-1) && status[i-1][j] == null){
-            System.out.println("good move2");
+            currentMessage="OK";
             status[i-1][j] = status[i][j];
             status[i][j] = null;
+
+            //set the blank variables
+            xemp = i;
+            yemp = j;
         }
         else if(inBounds(j+1) && status[i][j+1]== null){
-            System.out.println("good move3");
+            currentMessage="OK";
             status[i][j+1] = status[i][j];
             status[i][j] = null;
+
+            //set the blank variables
+            xemp = i;
+            yemp = j;
         }
         else if(inBounds(j-1) && status[i][j-1]== null){
-            System.out.println("good move4");
+            currentMessage="OK";
             status[i][j-1] = status[i][j];
             status[i][j] = null;
+
+            //set the blank variables
+            xemp = i;
+            yemp = j;
         }
 
         else{
-            System.out.println("nah");
+            currentMessage="Please choose a position next to the empty one!";
+            return false;
         }
 
-        System.out.println(inBoundsX);
-        return false;
+
+        return true;
     }
 
     @Override
@@ -80,6 +111,6 @@ public class FifteenModel implements Boardgame {
 
     @Override
     public String getMessage() {
-        return null;
+        return currentMessage;
     }
 }
